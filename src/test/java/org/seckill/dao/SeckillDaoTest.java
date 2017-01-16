550c010573b351f086d8d1a5f7c849fa58afa5f5
yz1873 <yz1873@126.com>
2017-01-16 22:47:23 +0800

    junit测试阶段

---
+++ src/test/java/org/seckill/dao/SeckillDaoTest.java
@@ -0,0 +1,44 @@
+package org.seckill.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.seckill.entity.Seckill;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+import static org.junit.Assert.*;
+
+/**
+ * 配置spring和junit整合，junit启动时加载springIOC容器
+ * spring-test,junit
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+//告诉junit spring配置文件
+@ContextConfiguration({"classpath:spring/spring-dao.xml"})
+public class SeckillDaoTest {
+
+    //注入Dao实现类依赖
+    @Resource
+    private SeckillDao seckillDao;
+
+    @Test
+    public void testQueryById() throws Exception {
+        long seckillId=1000;
+        Seckill seckill=seckillDao.queryById(seckillId);
+        System.out.println(seckill.getName());
+        System.out.println(seckill);
+    }
+
+    @Test
+    public void testQueryAll() throws Exception {
+
+    }
+
+    @Test
+    public void testReduceNumber() throws Exception {
+
+    }
+
+}
