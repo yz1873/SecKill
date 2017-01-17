83787a565cbd5c8c209d065dc33723b55a0f603e
yz1873 <yz1873@126.com>
2017-01-17 16:58:39 +0800

    事务管理和配置完成

--- src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
+++ src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
@@ -13,6 +13,9 @@ import org.seckill.exception.SeckillException;
 import org.seckill.service.SeckillService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.DigestUtils;
 
 import java.util.Date;
@@ -21,13 +24,17 @@ import java.util.List;
 /**
  * Created by Zhang Yu on 2017/1/17.
  */
+@Service
 public class SeckillServiceImpl implements SeckillService {
 
     //日志对象
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    //注入service依赖
+    @Autowired
     private SeckillDao seckillDao;
 
+    @Autowired
     private SuccessKilledDao successKilledDao;
 
     //md5盐值字符串，用于混淆MD5
@@ -69,6 +76,15 @@ public class SeckillServiceImpl implements SeckillService {
     }
 
     @Override
+    @Transactional
+    /**
+     * 事务：指访问并可能更新数据库中各种数据项的一个程序执行单元(unit)
+     * 特点：要不全执行，要不都不执行——不报错就commit，报错就执行回滚
+     * 使用注解控制事务方法的优点:
+     * 1.开发团队达成一致约定，明确标注事务方法的编程风格。
+     * 2.保证事务方法的执行时间尽可能短，不要穿插其他网络操作RPC/HTTP请求或者剥离到事务方法外部。
+     * 3.不是所有的方法都需要事务，如只有一条修改操作、只读操作不要事务控制。
+     */
     public SeckillExecution executeSeckill(long seckillId, long userPhone, String md5) throws SeckillException, SeckillCloseException, RepeatKillException {
         if (md5 == null || !md5.equals(getMD5(seckillId))) {
             throw new SeckillException("seckill data rewrite");
